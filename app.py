16f745270cec5b0770d975231121ac2ec418b456
alexander.verbeeck <alexander.verbeeck@student.ucll.be>
2025-12-15 16:27:00 +0100

    Dit is temp

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ def main():
 
 
     def create_main_surface():
-        #initialize Pygame
-        pygame.init()
+        pygame.init() #initialize Pygame
+        screen_size = (1024, 768) # Tuple representing width and height in pixels
+        screen = pygame.display.set_mode(screen_size)
+        return screen
+
+    screen = create_main_surface() # Create window with given size
+    balloon = pygame.image.load("ballon.png")
+    balloon = pygame.transform.scale(balloon, (100, 150))  # breedte, hoogte
 
 
-        # Tuple representing width and height in pixels
-        screen_size = (1024, 768)
-        return screen_size
-
-    screen_size = create_main_surface()
-    # Create window with given size
-    while True:
-        pygame.display.set_mode(screen_size)
+    
+    running = True
+    while running:
+        for event in pygame.event.get(): # om het spel af te sluiten
+            if event.type == pygame.QUIT:# dit ook
+                running = False # dit ook
+        screen.fill((50, 100, 180)) # de achtergrond een kleur geven
+        screen.blit(balloon, (450, 500)) # de afbeelding op het scherm zetten
+        pygame.display.flip() # ververst het scherm
+    pygame.quit() # ook nog om het spel af te sluiten
 
 main()
